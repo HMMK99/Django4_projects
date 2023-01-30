34b44c1490fa5c28e07bd556aa5033461b2be1f7
HMMK99 <hatemkamal1999@gmail.com>
2023-01-30 20:05:33 +0200

    adding_SEO_friendly_URLS

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -17,7 +17,8 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
     auther = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_post')
@@ -42,4 +43,7 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',
-                       args=[self.id])
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
